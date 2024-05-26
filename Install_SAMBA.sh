fd778c279204b87775fc31c0d17101e71d83fc08
Fguillouet <franck.guillouet@outlook.fr>
2024-05-26 22:45:54 +0200

    Update Install_SAMBA.sh

--- Install_SAMBA.sh
+++ Install_SAMBA.sh
@@ -1,20 +1,36 @@
 #/bin/sh
+err_catch() {
+   echo "une erreur à été détectée, nettoyage en cours "
+   rmdir $CheminComplet
+   userdel $NomUtilisateur
+   groupedel $NomGroupe
+   rm /etc/samba/smb.conf
+   mv /etc/samba/smb.conf.bck /etc/samba/smb.conf
+   exit 1
+}
 
 # Initialisation variable
 ip4=$(ip addr show ens18 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 # Update & installation de SAMBA
-sudo apt update
-sudo apt install -y samba
-sudo systemctl enable smbd
+apt_samba() {
+   sudo apt update
+   sudo apt install -y samba
+   sudo systemctl enable smbd
+   cp /etc/samba/smb.conf /etc/samba/smb.conf.bck
+}
 
 # Création d'un partage SAMBA
 clear
+read -p "Besoin d'installer Samba ? : (Y/N)" InstallSamba
 read -p "Entrer le nom du partage samba : " NomPartage
 read -p "Entrez un nom pour le nouvel utilisateur : " NomUtilisateur
 read -p "Entrez un nom pour le groupe associé au partage : " NomGroupe
 read -p "Quel est le chemin du partage : " CheminComplet
 
+if (InstallSamba=Y); then
+   apt_samba
+fi
 # Création du dossier partagé
 clear
 echo "step 1"
@@ -24,9 +40,9 @@ echo "Dossier créé"
 # Création de l'utilisateur et asignation au groupe (système + samba)
 echo "step 2"
 useradd $NomUtilisateur
-echo "utilisateur créé"
 passwd $NomUtilisateur
 smbpasswd -a $NomUtilisateur
+echo "Utilisateur créé"
 
 # Création du groupe
 echo "step 3"
@@ -42,7 +58,7 @@ echo "Permissions appliquées"
 
 # Création et import de la configuration SAMBA
 echo "step 5"
-tee -a /etc/samba/smb.conf << END
+tee -a /etc/samba/smb.conf <<END
 [$NomPartage]
    comment = Partage de données
    path = $CheminComplet
@@ -55,4 +71,4 @@ sudo systemctl restart smbd
 
 # Fin du script
 echo "L'opération est terminée !!"
-echo "Le partage est disponible à l'adresse $ip4 sous le nom $NomPartage" 
+echo "Le partage est disponible à l'adresse $ip4 sous le nom $NomPartage"
